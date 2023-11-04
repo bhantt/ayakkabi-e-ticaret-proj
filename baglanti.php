f805417a5584c5bae927ada616fef1210341dd21
batuhanDev <batuhantandkay@gmail.com>
2023-11-04 22:02:25 +0300

    HUGE UPDATE!
    virtual box kullanılarak bir mysql server oluşturuldu uzaktan bağlanıldı ayarlar yapıldı. artık verileri bir sunucuda. tek yapmanız gereken benden virtual boxdaki bilgisayarımı başlatmamı istemeniz.

---
+++ baglanti.php
@@ -0,0 +1,21 @@
+<?php
+
+$sunucu = "192.168.1.101";
+$database = "eticaret";
+$username = "pardusbatu";
+$password = "123456";
+
+
+$conn = mysqli_connect($sunucu, $username, $password, $database);
+
+if(!$conn){
+
+   die("Baglanti Hatasi!" . mysqli_conenct_error());
+
+}
+echo "Baglanti Basarili!";
+
+
+
+
+?>
